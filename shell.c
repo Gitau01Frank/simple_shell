6f02edbffab8f2d9519bc3f027665883fecc03ba
Francis Wainaina <franciswainainag@gmail.com>
2022-09-07 16:47:26 +0300

    Getting better.

--- shell.c
+++ shell.c
@@ -58,3 +58,24 @@ void printDir()
 void execArgs(char** parsed)
 {
 	// Forking a child
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		printf("\nFailed forking child..");
+		return;
+	} else if (pid == 0) {
+		if (execvp(parsed[0], parsed) < 0) {
+			printf("\nCould not execute command..");
+		}
+		exit(0);
+	} else {
+		// waiting for child to terminate
+		wait(NULL);
+		return;
+	}
+}
+
+// Function where the piped system commands is executed
+void execArgsPiped(char** parsed, char** parsedpipe)
+{
+
