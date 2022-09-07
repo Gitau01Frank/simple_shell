94de10e4083d0de516b6541c3738c5c3ee1d297e
Francis Wainaina <franciswainainag@gmail.com>
2022-09-07 16:44:41 +0300

    Adding more.

--- shell.c
+++ shell.c
@@ -18,3 +18,24 @@
 void init_shell()
 {
 	clear();
+	printf("\n\n\n\n******************"
+		"************************");
+	printf("\n\n\n\t****MY SHELL****");
+	printf("\n\n\t-USE AT YOUR OWN RISK-");
+	printf("\n\n\n\n*******************"
+		"***********************");
+	char* username = getenv("USER");
+	printf("\n\n\nUSER is: @%s", username);
+	printf("\n");
+	sleep(1);
+	clear();
+}
+
+// Function to take input
+int takeInput(char* str)
+{
+	char* buf;
+
+	buf = readline("\n>>> ");
+	if (strlen(buf) != 0) {
+
